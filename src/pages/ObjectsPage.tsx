bce916148888b0f31488455235f276c474f070db
Jennifer Lu <jenniferlugm@gmail.com>
2024-01-02 23:52:52 -0500

    edit photos

--- src/pages/ObjectsPage.tsx
+++ src/pages/ObjectsPage.tsx
@@ -3,22 +3,10 @@ import PageWrapper from "../components/PageWrapper";
 
 const images = [
   {
-    src: "images/photos/IMG_4898.jpeg",
+    src: "images/photos/cessy_og.jpeg",
     width: 4032,
     height: 3024,
     alt: "Cessy, France; December 2023",
-  },
-  {
-    src: "images/photos/IMG_8896.jpeg",
-    width: 4032,
-    height: 3024,
-    alt: "Sutro Baths; February 2023",
-  },
-  {
-    src: "images/photos/IMG_8902.jpeg",
-    width: 3024,
-    height: 4032,
-    alt: "Sutro Baths; February 2023",
   },
 ];
 
